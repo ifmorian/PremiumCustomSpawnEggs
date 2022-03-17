0466b37fcfb8df89a636093f104b735768e8c26b
Orian <lol.orian2010@gmail.com>
2022-03-17 11:26:24 +0100

    Initial commit

---
+++ src/main/java/de/felix_kurz/premiumcustomspawneggs/entities/CustomMob.java
@@ -0,0 +1,10 @@
+package de.felix_kurz.premiumcustomspawneggs.entities;
+
+import org.bukkit.entity.EntityType;
+
+public class CustomMob {
+
+    private String name;
+    private EntityType type;
+
+}
---
+++ src/main/java/de/felix_kurz/premiumcustomspawneggs/main/Main.java
@@ -0,0 +1,15 @@
+package de.felix_kurz.premiumcustomspawneggs.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public void onEnable() {
+        saveDefaultConfig();
+    }
+
+    public void onDisable() {
+
+    }
+
+}
