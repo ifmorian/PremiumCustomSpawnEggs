8587ef40a8aeebf6c4a827f9408aff62bab5d9ff
Orian <felix.kurz2010@gmail.com>
2022-03-25 03:30:10 +0100

    nix

--- src/main/java/de/felix_kurz/premiumcustomspawneggs/entities/nmsentities/CustomZombie.java
+++ src/main/java/de/felix_kurz/premiumcustomspawneggs/entities/nmsentities/CustomZombie.java
@@ -1,13 +1,14 @@
 package de.felix_kurz.premiumcustomspawneggs.entities.nmsentities;
 
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.Zombie;
-import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_18_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_18_R2.entity.CraftLivingEntity;
 
 public class CustomZombie extends Zombie {
-    public CustomZombie(Level world, Location l) {
-        super(world);
-        this.spawnAtLocation((ItemLike) l);
+    public CustomZombie(Location l) {
+        super(EntityType.ZOMBIE, ((CraftWorld) l.getWorld()).getHandle());
     }
 }
